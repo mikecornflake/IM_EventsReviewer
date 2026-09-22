eb357b1e59b707f647583c2b7075fba2359b350c
Mike Cornflake <mike.cornflake@gmail.com>
2026-09-22 23:36:45 +0100

    fix

--- Display/FormEventsReviewer.pas
+++ Display/FormEventsReviewer.pas
@@ -605,7 +605,7 @@ Begin
   Else
     sFolder := FSettings.AnomalyImageFolder;
 
-  If sFolder=FLastImageFolder Then
+  If *AAnomalyReference='') And (sFolder=FLastImageFolder) Then
     Exit;
 
   FLastImageFolder := sFolder;
